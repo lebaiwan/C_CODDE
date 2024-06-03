7d503c2026236362d186873bdea0636cf4fba016
lebaiwan <1153542644@qq.com>
2024-06-03 14:31:00 +0800

    Add files via upload

---
+++ test01/test01/test.c
@@ -0,0 +1,177 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+////#include<string.h>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//
+//	scanf("%d %d", &num1, &num2);
+//
+//	int sum = num1 + num2;
+//
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//
+//	char arr1[] = "abcdef";
+//	char arr2[] = { 'a','b','c','d','e','f','\0'};
+//	/*printf("%s\n", arr1);
+//	printf("%s\n", arr2);*/
+//
+//	int len = strlen("abc");
+//	printf("%d\n", len);
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//
+//	
+//
+//	return 0;
+//}
+
+
+//输出一辆小飞机
+//int main()
+//{
+//	printf("     **\n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *\n");
+//	printf("    *  *\n");
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("name    age    gender\n");
+//	printf("---------------------\n");
+//	printf("jack    18     man\n");
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 40;
+//	int c = 212;
+//	int y = (-8 + 22) * a - 10 + c / 2;
+//	printf("%d\n", y);
+//
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d %d", &x);
+//
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	
+//	int age = 0;
+//	scanf("%d", &age);
+//
+//	if (age <= 10)
+//	{
+//		printf("这是小孩儿\n");
+//		printf("小孩儿不许学习\n");
+//	}
+//	else if (age > 10 && age <= 18)
+//	{
+//		printf("这是出生\n");
+//		printf("鲨鲨鲨\n");
+//	}
+//	else if (age > 18 && age <= 35)
+//	{
+//		printf("这是社畜\n");
+//	}
+//	else
+//		printf("这是老登\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a >= 1 && a <= 100)
+//	{
+//		if (a % 2 == 1)
+//		{
+//			printf("有效\n");
+//		}
+//		else
+//		{
+//			printf("无效\n");
+//		}
+//
+//	}
+//	else
+//	{
+//		printf("无效\n");
+//	}
+//
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	if (a >= 1 && a <= 100)
+//	{
+//
+//	}
+//	printf("%d", a);
+//
+//	return 0;
+//}
+
+#include<stdio.h>
+//int main()
+//{
+//	char ch;
+//	scanf("%c", &ch);
+//	printf("  %c\n", ch );
+//	printf(" %c%c%c\n", ch, ch, ch);
+//	printf("%c%c%c%c%c\n", ch, ch, ch, ch, ch);
+//
+//	return 0;
+//}
+
+
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+
+	while (i<=10)
+	{
+		printf("%d\n", i);
+
+		i++;
+		
+	}
+
+	return 0;
+}
